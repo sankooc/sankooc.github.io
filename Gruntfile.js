0d49d9d006f58d24c9ccee89c448d650a8dbeeb4
sankooc <sankooc@163.com>
2015-06-22 14:24:57 +0800

    init commit

---
+++ Gruntfile.js
@@ -0,0 +1,158 @@
+'use strict';
+
+module.exports = function (grunt) {
+  var path = require('path');
+  
+  var dep = [
+    'src/vendor/js/*.js'
+  ];
+
+  var dest = 'dest';
+  var csses = [
+    'src/css/*.css'
+  ];
+  var vendors = [];
+  vendors.push('src/vendor/js/modernizr-2.0.js');
+  var _files = [
+    {
+      expand: true,
+      cwd: 'bower_components/',
+      src: '**', 
+      dest: 'dest/vendor/'
+    },{
+      expand: true,
+      cwd: 'src/css/',
+      src: '**', 
+      dest: 'dest/css/'
+    }
+  ];
+  
+  var _vendor = {
+    src:[],
+    dest: 'dependence.js'
+  }
+  for(var i=0;i<vendors.length;i++){
+    var v = vendors[i];
+    // var fname = path.basename(v);
+    _vendor.src.push(v);
+    // _files.push({
+//       src:
+//     });
+  }
+  // _files.push(_vendor);
+  grunt.initConfig({
+      // Metadata.
+      pkg: grunt.file.readJSON('package.json')
+
+      ,copy: {
+        main: {
+          files: _files
+        }
+      }
+
+      ,less: {
+        compile: {
+          options: {
+            paths: ['src/css/include']
+          },
+          files: {
+            'dest/css/main.css': 'src/css/*.less'
+          }
+        }
+      }
+
+      ,concat_sourcemap: {
+        options: {
+          banner: '<%= banner %>',
+          sourcesContent: true,
+          stripBanners: true
+        }
+        , script: {
+          files: {
+            'dest/js/vendor.js': dep
+          }
+        }
+      }
+      
+      ,coffee: {
+        compile: {
+          options: {
+            sourceMap: true
+          },
+          files: {
+            'dest/js/app.js': ['src/js/*.coffee', 'src/js/*/*.coffee']
+          }
+        }
+      }
+
+      , html2js: {
+        main: {
+          src: ['src/view/*.html'],
+          dest: 'dest/html2js.js'
+        }
+      }
+
+      ,'http-server':{
+        dev: {
+          // the server root directory
+          root: '.',
+          port: 8001,
+          // port: function() { return 8282; }
+          host: "127.0.0.1",
+          cache: 1000,
+          showDir : true,
+          autoIndex: true,
+          // server default file extension
+          ext: "html",
+          // run in parallel with other tasks
+          runInBackground: true
+        }
+
+      }
+
+      , watch: {
+        html : {
+          files:'src/main.html',
+          task : ['copy:main']
+        },
+        html2js : {
+          files:'src/view/*.html',
+          task : ['html2js:main']
+        },
+        jsfile: {
+          files: 'src/js/*.js',
+          tasks: ['concat_sourcemap:script']
+        }
+        , jsfile2: {
+          files: 'src/js/*/*.js',
+          tasks: ['concat_sourcemap:script']
+        }
+        ,coffee:{
+          files: ['src/js/*.coffee', 'src/js/*/*.coffee'],
+          tasks: ['coffee:compile']
+        }
+        , css: {
+          files: 'src/css/*.less',
+          tasks: ['less:compile']
+        }
+      }
+
+    }
+  )
+  ;
+
+// These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-concat-sourcemap');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-html2js');
+  grunt.loadNpmTasks('grunt-http-server');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+// Default task.
+  // grunt.registerTask('build', ['copy','less', 'concat_sourcemap','coffee', 'html2js','http-server','watch']);
+  grunt.registerTask('build', ['copy','less', 'concat_sourcemap','coffee','http-server','watch']);
+  grunt.registerTask('default', ['build']);
+}
+;
---
+++ src/js/app.coffee
@@ -0,0 +1,106 @@
+angular.module("app",['ui.router','angular-loading-bar'])
+.config ($stateProvider, $urlRouterProvider,cfpLoadingBarProvider)->
+  cfpLoadingBarProvider.includeBar = true
+  prefix = 'src/'
+  $stateProvider
+  .state 'blogs', {
+    url: '/blogs/:page'
+    controller:'BlogsController'
+    templateUrl: prefix+'view/blogs.html'
+  }
+  .state 'blog', {
+    url: '/blog/:id'
+    controller:'BlogController'
+    templateUrl: prefix+'view/blog.html'
+  }
+  .state 'archives', {
+    url: '/archives'
+    controller:'ArchivesController'
+    templateUrl: prefix+'view/archives.html'
+  }
+  $urlRouterProvider.otherwise('/blogs/1');
+.run ($rootScope,$http)->
+  $rootScope.title = 'Sankooc'
+.service 'IssueService',($q,cfpLoadingBar)->
+  cache = null
+  _load = ()->
+    cfpLoadingBar.start()
+    user = new Gh3.User('sankooc')
+    repo = new Gh3.Repository('sankooc.github.io',user)
+    q = $q.defer()
+    repo.fetchIssues (err,data)->
+      cfpLoadingBar.complete()
+      if err 
+        q.reject err
+      else
+        cache = data
+        q.resolve(data)
+    q.promise
+  @load = (force)->
+    if force
+      return _load()
+    else if cache
+      q = $q.defer()
+      q.resolve(cache)
+      q.promise
+    else
+      return _load()
+  this
+.controller 'BlogsController',($scope,IssueService,$stateParams)->
+  $scope.blogs = []
+  $scope.page = parseInt($stateParams.page)
+  per = 2
+  inx = ($scope.page-1)*per
+  converter = new showdown.Converter()
+  IssueService.load(true).then (data)->
+    console.log data
+    _.each data.issues, (issue)->
+      if $scope.blogs.length >= per
+        return
+      if issue.milestone.title is 'blog'
+        if inx > 0
+          inx--
+          return
+        body = issue.body
+        content = converter.makeHtml body
+        issue.trim = trimHtml(content,{limit:200})
+        $scope.blogs.push issue
+  $scope.toHtml = (body)->
+    content = converter.makeHtml body
+    trimHtml(content,{limit:200})
+.controller 'BlogController',($scope,IssueService,$stateParams)->
+  converter = new showdown.Converter()
+  id = $stateParams.id
+  issues = null
+  IssueService.load().then (data)->
+    issues = data
+    _.each data.issues, (issue,index,arr)->
+      if issue.milestone.title is 'blog'
+        if issue.id is parseInt id
+          console.log(issue)
+          $scope.blog = issue
+          $scope.body = converter.makeHtml issue.body
+          if arr.length >= index+2
+            $scope.next = arr[index+1]
+            console.log $scope.next
+          return
+.controller 'ArchivesController',($scope,IssueService)->
+  $scope.blogs = []
+  map = $scope.map = {}
+  IssueService.load(true).then (data)->
+    _.each data.issues, (issue)->
+      if issue.milestone.title is 'blog'
+        $scope.blogs.push issue
+    _.each $scope.blogs,(blog)->
+      _date = moment(blog.updated_at)
+      y = _date.format('YYYY')
+      if not map[y]
+        map[y] = []
+      _s1 = map[y]
+      _s1.push blog
+    $scope.years = Object.keys map
+    
+.filter('htmlSafe', ['$sce', ($sce)->
+  (text)->
+    $sce.trustAsHtml(text)
+])
